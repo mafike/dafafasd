3d6b0134e09a0c53942b4c4961192b0e55471a71
mafike <mafgenes1@icloud.com>
2024-03-22 16:57:24 +0000

    c1

---
+++ jenkins_installation.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre -y
+sudo install wget
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo apt-get install git maven -y
+
